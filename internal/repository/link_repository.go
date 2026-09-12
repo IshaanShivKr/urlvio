e79c605f3500d818316912f9b9f87fa60283ae09
Ishaan Shiv Kumar <ishaanshivkr@gmail.com>
2026-09-12 17:51:22 +0530

    define link repository contract

---
+++ internal/repository/link_repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/IshaanShivKr/urlvio/internal/model"
+)
+
+type LinkRepository interface {
+	Create(ctx context.Context, link *model.Link) error
+	Get(ctx context.Context, code string) (*model.Link, error)
+	Update(ctx context.Context, link *model.Link) error
+	Delete(ctx context.Context, code string) error
+	GetAndIncrement(ctx context.Context, code string) (*model.Link, error)
+}
