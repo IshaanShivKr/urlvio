13cdd1a4addda77277500f0fbf21e53898e6eb9d
Ishaan Shiv Kumar <ishaanshivkr@gmail.com>
2026-09-14 13:57:50 +0530

    allow Vite dev server origin

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -68,8 +68,8 @@ func run() error {
 	router.Use(gin.Logger(), gin.Recovery())
 	router.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
-			"http://localhost:5500",
-			"http://127.0.0.1:5500",
+			"http://localhost:5173",
+			"http://127.0.0.1:5173",
 		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Authorization", "Content-Type"},
